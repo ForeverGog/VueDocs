dd0c8089c4a987d2e580eb0a9a14e316ededc333
AsheOne18 <89166015+AsheOne18@users.noreply.github.com>
2021-12-08 22:30:42 -0500

    更新导航栏

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,18 +1,11 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
+
   {
     text: '前端',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      // {
-      //   text: '前端文章',
-      //   items: [
-      //     { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-      //     // { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-      //   ],
-      // },
       {
         text: '学习笔记',
         items: [
@@ -26,21 +19,30 @@ module.exports = [
     text: '后端',
     link: '/ui/',
     items: [
-      { text: 'Java', link: '/pages/35e5b3/' },
-      { text: 'C++', link: '/pages/b3da01/' },
+      {
+        text: '学习笔记',
+        items: [
+          { text: '《Java》', link: '/note/java/' },
+          { text: '《C++》', link: '/note/cpp/' },
+        ],
+      },
     ],
   },
+  // ------------------------------------------------
   {
     text: '技术',
     link: '/technology/',
     items: [
-      { text: 'SSM', link: '/pages/00bef2/' },
-      { text: 'SpringBoot', link: '/pages/0d4b9f/' },
-      { text: 'SpringCloud', link: '/pages/96e573/' },
-      { text: 'Maven', link: '/pages/5b9927/' },
+{
+  text: '学习笔记',
+  items: [
+    {text: '《Maven》',link: '/note/maven/'},
+    {text: '《Spring5》',link: '/note/spring5/'},
+  ]
+}
     ],
   },
-
+  // -------------------------------------------------
   {
     text: '小记',
     link: '/more/',
